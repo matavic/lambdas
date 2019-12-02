43246fafdcbf839b271ad243f01e0448e7006438
Vicente Mata <matavic25@gmail.com>
2019-12-02 15:12:48 -0400

    Added OMDB Api search functionality and update model

--- src/lambda/awards.js
+++ src/lambda/awards.js
@@ -1,21 +1,20 @@
 var mongoose = require('mongoose');
-
+var axios = require('axios');
 let conn = null;
-
-const uri = 'mongodb+srv://matavic:8G2AtquEEtpP0Ihk@vmcluster-my0iu.mongodb.net/awardsseason?retryWrites=true&w=majority';
+const apiOMDB = 'http://www.omdbapi.com';
+const uri = 'mongodb+srv://' + process.env.MONGODB_ATLAS_USER + ':' + process.env.MONGODB_ATLAS_PASSWORD + '@vmcluster-my0iu.mongodb.net/' + process.env.MONGODB_ATLAS_DB_NAME + '?retryWrites=true&w=majority';
 
 exports.handler = function(event, context, callback) {
-
   context.callbackWaitsForEmptyEventLoop = false;
-
-  run().
+  const award = event.queryStringParameters.a;
+  run(award).
     then(res => {
       callback(null, res);
     }).
     catch(error => callback(error));
 };
 
-async function run() {
+async function run(a) {
 
     if (conn == null) {
       conn = await mongoose.createConnection(uri, {
@@ -28,13 +27,34 @@ async function run() {
         fnominees: Array,
         fwinner: Array,
         pnominees: Array,
-        pwinner: Array
+        pwinner: Array,
+        winnerdata: Object
       }));
     }
 
     const M = conn.model('awards');
+    let mod = false;
+    let doc = await M.find({ award: a });   
+    let resp = await doc.map( async function(f){
+      if(f.fwinner.length && Object.entries(f.winnerdata).length === 0){
+        try {
+          const r = await axios.get(apiOMDB, {
+            params: {
+              apikey: process.env.OMDB_API_KEY,
+              t: f.fwinner[0],
+              y: '2019'
+            }
+          });
+          f.winnerdata = r.data;
+          let res = await M.update({ _id: f._id }, { winnerdata: f.winnerdata });
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    });
 
-    const doc = await M.find();
+    const results = await Promise.all(resp);
+
     const response = {
       statusCode: 200,
       body: JSON.stringify(doc)
