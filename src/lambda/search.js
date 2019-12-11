67c7822a54ee9ede9e86ab4f3d3a82e10710e98e
Vicente Mata <matavic25@gmail.com>
2019-12-11 12:54:54 -0400

    Added search function

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -1,21 +1,19 @@
 var mongoose = require('mongoose');
-var axios = require('axios');
 let conn = null;
-const apiOMDB = 'http://www.omdbapi.com';
 const uri = 'mongodb+srv://' + process.env.MONGODB_ATLAS_USER + ':' + process.env.MONGODB_ATLAS_PASSWORD + '@vmcluster-my0iu.mongodb.net/' + process.env.MONGODB_ATLAS_DB_NAME + '?retryWrites=true&w=majority';
 
 exports.handler = function(event, context, callback) {
   context.callbackWaitsForEmptyEventLoop = false;
-  const award = event.queryStringParameters.a;
-  run(award).
+  const search = event.queryStringParameters.t;
+  run(search).
     then(res => {
       callback(null, res);
     }).
     catch(error => callback(error));
 };
 
-async function run(a) {
-  let b = a.split(',');
+async function run(s) {
+  let b = s.split(',');
     if (conn == null) {
       conn = await mongoose.createConnection(uri, {
         bufferCommands: false,
@@ -36,26 +34,12 @@ async function run(a) {
     }
 
     const M = conn.model('awards');
-    let doc = await M.find({ award: b[0] });   
-    let resp = await doc.map( async function(f){
-      if(f.fwinner.length && Object.entries(f.winnerdata).length === 0){
-        try {
-          const r = await axios.get(apiOMDB, {
-            params: {
-              apikey: process.env.OMDB_API_KEY,
-              t: f.fwinner[0],
-              y: '2019'
-            }
-          });
-          f.winnerdata = r.data;
-          let res = await M.update({ _id: f._id }, { winnerdata: f.winnerdata });
-        } catch (err) {
-          console.error(err);
-        }
-      }
-    });
-    const results = await Promise.all(resp);
-;    const response = {
+    let doc = await M.find({ $or: [
+        { fnominees: { $elemMatch : { $regex: b[0], $options: "i" }}},
+        { pnominees: { $elemMatch : { $regex: b[0], $options: "i" }}}
+      ]});   
+    const results = await Promise.all(doc);
+    const response = {
       statusCode: 200,
       headers: {
         'content-type': 'application/json',
