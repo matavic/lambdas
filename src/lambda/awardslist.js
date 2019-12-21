67b2379e86cc6809cc35bfeb11a035a6445f13db
Vicente Mata <matavic25@gmail.com>
2019-12-21 18:30:56 -0400

    Added awardslist function

---
+++ src/lambda/awardslist.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+let conn = null;
+const uri = 'mongodb+srv://' + process.env.MONGODB_ATLAS_USER + ':' + process.env.MONGODB_ATLAS_PASSWORD + '@vmcluster-my0iu.mongodb.net/' + process.env.MONGODB_ATLAS_DB_NAME + '?retryWrites=true&w=majority';
+
+exports.handler = function(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+  run().
+    then(res => {
+      callback(null, res);
+    }).
+    catch(error => callback(error));
+};
+
+async function run() {
+  if (conn == null) {
+    conn = await mongoose.createConnection(uri, {
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    conn.model('awardslist', new mongoose.Schema({
+      title: String,
+      image: String,
+      description: String,
+      date: Array,
+    }));
+  }
+
+  const M = conn.model('awardslist');
+  let doc = await M.find();   
+  const response = {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(doc)
+  };
+  return response;
+}
