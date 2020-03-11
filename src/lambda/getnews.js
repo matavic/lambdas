feb4e73e64c58ee47a2912e93953bd783e8aecef
Vicente Mata <matavic25@gmail.com>
2020-03-11 15:25:28 -0400

    Added getnews and useractionsv2 functions

---
+++ src/lambda/getnews.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+let conn = null;
+// const uri = 'mongodb+srv://' + process.env.MONGODB_ATLAS_USER + ':' + process.env.MONGODB_ATLAS_PASSWORD + '@vmcluster-my0iu.mongodb.net/' + process.env.MONGODB_ATLAS_DB_NAME + '?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://matavic:8G2AtquEEtpP0Ihk@vmcluster-my0iu.mongodb.net/awardsseason?retryWrites=true&w=majority';
+exports.handler = function(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+  run().
+    then(res => {
+      callback(null, res);
+    }).
+    catch(error => callback(error));
+};
+
+async function run() {
+    if (conn == null) {
+      conn = await mongoose.createConnection(uri, {
+        bufferCommands: false,
+        bufferMaxEntries: 0,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      conn.model('news', new mongoose.Schema({
+        id: String,
+        title: String,
+        header: String,
+        image: String,
+        source: String,
+        sourcethumbnail: String,
+        type: String,
+        link: String,
+        time: Number
+      }));
+    }
+    let doc;
+    const N = conn.model('news');
+    doc = await N.find({}).sort('-time').limit(15);
+    // const results = await Promise.all(resp);
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(doc)
+    };
+    return response;
+}
---
+++ src/lambda/useractionsv2.js
@@ -0,0 +1,311 @@
+var mongoose = require('mongoose');
+let conn = null;
+// const uri = 'mongodb+srv://' + process.env.MONGODB_ATLAS_USER + ':' + process.env.MONGODB_ATLAS_PASSWORD + '@vmcluster-my0iu.mongodb.net/' + process.env.MONGODB_ATLAS_DB_NAME + '?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://matavic:8G2AtquEEtpP0Ihk@vmcluster-my0iu.mongodb.net/awardsseason?retryWrites=true&w=majority';
+exports.handler = function(event, context, callback) {
+
+  // if(event.httpMethod === 'OPTIONS') {
+  //   const response = {
+  //     statusCode: 200,
+  //     headers: {
+  //       "Content-type": "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Methods" : "GET, POST, OPTIONS, PUT",
+  //       "Access-Control-Allow-Headers": "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+  //     },
+  //     body: 'OK'
+  //   }
+  //   callback(null, response)
+  //   return
+  // }
+  context.callbackWaitsForEmptyEventLoop = false;
+  // const { clientContext } = context;
+  // const username = clientContext.user ? clientContext.user.user_metadata.full_name : "guest";
+  // const useremail = clientContext.user ? clientContext.user.email : "guest";
+  
+  const action = event.queryStringParameters.a;
+  const flag = event.queryStringParameters.f;
+  const uemail = event.queryStringParameters.u;
+  const payload = event.queryStringParameters.p;
+  const item = JSON.parse(payload);
+  run(uemail, action, flag, item).
+    then(res => {
+      callback(null, res);
+    }).
+    catch(error => callback(error));
+};
+
+async function run(ue, a, f, it) {
+  let b = a.split(',');
+  let c = f.split(',');
+  let uem = ue ? ue.split(',')[0] : '';
+  let doc;
+  let response;
+  if((!uem)) {
+    doc = {
+      status: 'error',
+      message: 'Forbidden'
+    };
+    response = {
+      statusCode: 403,
+      headers: {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(doc)
+    };
+    return response;
+  }
+  if (conn == null) {
+    conn = await mongoose.createConnection(uri, {
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    conn.model('events', new mongoose.Schema({
+      award: String,
+      categorycod: String,
+      categorytit: String,
+      fnominees: Array,
+      fwinner: Array,
+      pnominees: Array,
+      pwinner: Array,
+      winnerdata: Object,
+      pnomineesdata: Array,
+      fnomineesdata: Array
+    }));
+    conn.model('users', new mongoose.Schema({
+      name: String,
+      email: String,
+      watched: Array,
+      watchlist: Array,
+      favorites: Array,
+      ratings: Array,
+      votes: Array
+    }));
+    conn.model('fasratings', new mongoose.Schema({
+      id: String,
+      title: String,
+      rating: String,
+      s: mongoose.Schema.Types.Decimal128,
+      c: Number,
+    }));
+  }
+
+    const U = conn.model('users');
+    // let searchfor = e !== 'guest' ? e : uem;
+    let user = await U.findOne({ email: uem }); 
+    
+    if(!user) {
+      doc = {
+        status: 'error',
+        message: 'User not found'
+      };
+      response = {
+        statusCode: 404,
+        headers: {
+          'Content-type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(doc)
+      };
+      return response;  
+    }
+    let re;
+    let encontrado;
+    // let encontradoWatchlist;
+    // let encontradoWatched;
+    let wm;
+    try {
+      switch (b[0]) {
+        case 'watched':
+          encontrado = user.watched.findIndex(film => film.Title === it.Title);
+          if(c[0] === 'i' && encontrado !== -1) {
+            wm = await U.update({ _id: user._id, "watchlist.Title": it.Title }, { $pull: { watchlist: { Title:  it.Title } } });
+            doc = {
+              status: 'success',
+              message: 'Register not modified'
+            };  
+            response = {
+              statusCode: 200,
+              headers: {
+                'content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+              },
+              body: JSON.stringify(doc)
+            };
+            return response;  
+          } else {
+            re = c[0] === 'i' ? await U.update({ _id: user._id }, { $push: { watched: it } }) : await U.update({ _id: user._id }, { $pull: { watched: { Title:  it.Title } } });
+            if(c[0] === 'i')
+              wm = await U.update({ _id: user._id, "watchlist.Title": it.Title }, { $pull: { watchlist: { Title:  it.Title } } });
+          }
+          break;
+        case 'favorites':
+          encontrado = user.favorites.findIndex(f => f.Title === it.Title);
+          if(c[0] === 'i' && encontrado !== -1) {
+            doc = {
+              status: 'success',
+              message: 'Register not modified'
+            };  
+            response = {
+              statusCode: 200,
+              headers: {
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+              },
+              body: JSON.stringify(doc)
+            };
+            return response;
+          } else {
+            re = c[0] === 'i' ? await U.update({ _id: user._id }, { $push: { favorites: it } }) : await U.update({ _id: user._id }, { $pull: { favorites: { Title: it.Title } } });
+          }  
+          break;
+        case 'watchlist':
+          encontrado = user.watchlist.findIndex(f => f.Title === it.Title);
+          if(c[0] === 'i' && encontrado !== -1) {
+            wm = await U.update({ _id: user._id, "watched.Title": it.Title }, { $pull: { watched: { Title:  it.Title } } });
+            doc = {
+              status: 'success',
+              message: 'Register not modified'
+            };  
+            response = {
+              statusCode: 200,
+              headers: {
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+              },
+              body: JSON.stringify(doc)
+            };
+            return response;
+          } else {
+            re = c[0] === 'i' ? await U.update({ _id: user._id }, { $push: { watchlist: it } }) : await U.update({ _id: user._id }, { $pull: { watchlist: { Title: it.Title } } });
+            if(c[0] === 'i')
+              wm = await U.update({ _id: user._id, "watched.Title": it.Title }, { $pull: { watched: { Title:  it.Title } } });
+          }  
+          break;
+          case 'ratings':
+            let oldrating = null;
+            let resul;
+            encontrado = user.ratings.findIndex(f => f.title === it.title);
+            if(encontrado !== -1) {
+              oldrating = user.ratings[encontrado].rating;
+            }
+            const R = conn.model('fasratings');
+            let movieRating = await R.findOne({ title: it.title }); 
+            if(!movieRating) {
+              let newRating = new R(
+                { 
+                  id: it.id ? it.id : '',
+                  title: it.title, 
+                  rating: String(it.rating),
+                  s: Number(it.rating),
+                  c: 1,
+                 }
+                );
+              let saved = await newRating.save(); 
+            } else {
+              let newS;
+              let newR;
+              if(encontrado !== -1){
+                newS = ( parseFloat(movieRating.s) - parseFloat(oldrating) ) + parseFloat(it.rating);
+                newR = String(newS / parseFloat(movieRating.c));
+                resul = await movieRating.update( { $set: { s: newS, rating: newR } });
+              } else {
+                newS = parseFloat(movieRating.s) + parseFloat(it.rating);
+                let count = parseFloat(movieRating.c);
+                count = count + 1;
+                newR = String(newS / count);
+                resul = await movieRating.update( { $set: { s: newS, rating: newR, c: count } });
+              }
+            }
+            re = c[0] === 'i' ? await U.update({ _id: user._id }, { $push: { ratings: it } }) : await U.update({ _id: user._id, "ratings.title": it.title }, { $set: { "ratings.$.rating": it.rating } });
+            break;
+        case 'votes':
+          let oldvote = null;
+          let oldvoteperson = null;
+          let oldvoteposition = null;
+          let resulting;
+          let resulting2;
+          encontrado = user.votes.findIndex(f => f.award === it.award && f.category === it.category);
+          
+          if(encontrado !== -1) {
+            oldvote = user.votes[encontrado].vote;
+            oldvoteperson = user.votes[encontrado].person;
+            oldvoteposition = user.votes[encontrado].position;
+          }
+          const A = conn.model('events');
+          let catVotes = await A.findOne({ award: it.award, categorytit: it.category });
+          let resp = [];
+          if(catVotes && catVotes.fnomineesdata.length > 0) {
+            let total = 0;
+            let ind;
+            catVotes.fnomineesdata.forEach(film => total = total + parseFloat(film.nvotes));
+            if(encontrado !== -1){
+              // ind = catVotes.fnomineesdata.findIndex(ct => ct.Title === oldvote);
+              ind = oldvoteposition;
+              // if(ind !== -1){
+                let fixOldN = parseFloat(catVotes.fnomineesdata[ind].nvotes) - 1;
+                let fixOldP = (fixOldN / total) * 100;
+                fixOldP = fixOldP === 0 || Number.isInteger(fixOldP) ? fixOldP : parseFloat(fixOldP.toFixed(1));
+                total = total - 1;
+                // resulting = await A.update( { award: it.award, categorytit: it.category, "fnomineesdata.Title": oldvote }, { $set: { "fnomineesdata.$.nvotes": fixOldN, "fnomineesdata.$.pvotes": fixOldP } });
+                resulting = await A.update( { award: it.award, categorytit: it.category }, { $set: { [`fnomineesdata.${ind}.nvotes`]: fixOldN, [`fnomineesdata.${ind}.pvotes`]: fixOldP } });
+              // } else {
+                // break;
+              // }
+            }
+            // let newInd = catVotes.fnomineesdata.findIndex(o => o.Title === it.vote);
+            let newInd = it.position;
+            let newN = parseFloat(catVotes.fnomineesdata[newInd].nvotes) + 1;
+            total = total + 1;
+            let newP = (newN / total) * 100;
+            newP = newP === 0 || Number.isInteger(newP) ? newP : parseFloat(newP.toFixed(1));
+            // resulting2 = await A.update( { award: it.award, categorytit: it.category, "fnomineesdata.Title": it.vote }, { $set: { "fnomineesdata.$.nvotes": newN, "fnomineesdata.$.pvotes": newP } });
+            resulting2 = await A.update( { award: it.award, categorytit: it.category }, { $set: { [`fnomineesdata.${newInd}.nvotes`]: newN, [`fnomineesdata.${newInd}.pvotes`]: newP } });
+            let newPercent;
+            for(let i = 0; i < catVotes.fnomineesdata.length; i++){
+              if(ind !== -1 && i === ind)
+                continue;
+              if(i === newInd)
+                continue;
+              newPercent = (parseFloat(catVotes.fnomineesdata[i].nvotes) / total) * 100;
+              newPercent = newPercent === 0 || Number.isInteger(newPercent) ? newPercent : parseFloat(newPercent.toFixed(1));
+              // resp[i] = await A.update( { award: it.award, categorytit: it.category, "fnomineesdata.Title": catVotes.fnomineesdata[i].Title }, { $set: { "fnomineesdata.$.pvotes": newPercent } });
+              resp[i] = await A.update( { award: it.award, categorytit: it.category }, { $set: { [`fnomineesdata.${i}.pvotes`]: newPercent } });
+            }
+            const resArray = await Promise.all(resp);
+          }
+          re = c[0] === 'i' ? await U.update({ _id: user._id }, { $push: { votes: it } }) : await U.update({ _id: user._id, "votes.award": it.award, "votes.category": it.category }, { $set: { "votes.$.vote": it.vote, "votes.$.person": it.person, "votes.$.position": it.position, "votes.$.id": it.id } });
+          break;        
+        default:
+          break;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    if(re && re.ok === 1){
+      user = await U.findOne({ email: uem });
+      doc = {
+        status: 'success',
+        message: 'Action complete',
+        user: user
+      }
+    } else {
+      doc = {
+        status: 'error',
+        message: 'Register not modified'
+      };  
+    }
+    
+    response = {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(doc)
+    };
+    return response;  
+}
